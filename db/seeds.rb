4d6e86aef96253cf7650b8cbd87fe7c17d16a899
Kevin Gandolfi <gandolfi.kevin@gmail.com>
2022-11-23 17:35:22 +0100

    Created show and index views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+restaurant = Restaurant.create({ name: 'Le Ritz', address: '31 rue des Poilus', phone_number: '06etc.', category: 'french' })
+Restaurant.create({ name: 'La tour d\'or', address: '31 rue des Tulipes', phone_number: '06etc.', category: 'french' })
 
-Restaurant.create({name: 'La tour d\'argent', address: '31 rue des Poilus', phone_number: '06etc.', category: 'french'})
-Restaurant.create({name: 'La tour d\'or', address: '31 rue des Tulipes', phone_number: '06etc.', category: 'french'})
+r = Review.new({ rating: 4, content: 'I would recommend it anytime' })
+r.restaurant = restaurant
+r.save!
